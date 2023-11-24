fdca2c888c157e5e476d3ea9fe955fd92a524d5b
Arnav Gupta <101815033+arnavgupta00@users.noreply.github.com>
2023-11-24 12:40:50 +0530

    Update TextToText.jsx

--- src/pages/TextToText/TextToText.jsx
+++ src/pages/TextToText/TextToText.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from '../../components/navbar/navbar';
 import { Link, useNavigate } from 'react-router-dom';
-import "dotenv/config";
 import './TextToText.css';
 
 
